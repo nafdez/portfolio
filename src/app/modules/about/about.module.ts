c02a686b9057f5f05fa4a76aac5ea804f6d53cb6
nafdez <fzpnacho@gmail.com>
2024-03-04 17:28:35 +0100

    About redesign

--- src/app/modules/about/about.module.ts
+++ src/app/modules/about/about.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './pages/about/about.component';
+import { StackCardComponent } from './components/stack-card/stack-card.component';
 
 
 @NgModule({
   declarations: [
-    AboutComponent
+    AboutComponent,
+    StackCardComponent
   ],
   imports: [
     CommonModule,
--- src/app/modules/about/pages/about/about.component.ts
+++ src/app/modules/about/pages/about/about.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Skill } from "@modules/about/components/stack-card/stack-card.component";
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styles: [
-  ]
+  selector: "app-about",
+  templateUrl: "./about.component.html",
+  styles: [],
 })
 export class AboutComponent {
-
+  frontendSkills: Skill[] = [
+    new Skill(
+      "HTML5",
+      "assets/images/projects/slot-machine/symbols/html_5.svg"
+    ),
+    new Skill("CSS3", "assets/images/projects/slot-machine/symbols/css_3.svg"),
+    new Skill(
+      "TypeScript",
+      "assets/images/projects/slot-machine/symbols/ts.svg"
+    ),
+    new Skill(
+      "Angular",
+      "assets/images/projects/slot-machine/symbols/angular.svg"
+    ),
+  ];
 }
