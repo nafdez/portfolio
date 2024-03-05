967fcf72c667018313eb21f642396362e586bb2e
nafdez <fzpnacho@gmail.com>
2024-03-06 00:46:40 +0100

    Internationalization working at it's best

--- src/app/shared/components/bento-card/bento-card.component.ts
+++ src/app/shared/components/bento-card/bento-card.component.ts
@@ -1,31 +1,12 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "bento-card",
   templateUrl: "./bento-card.component.html",
   styles: [],
 })
-export class BentoCardComponent implements OnInit {
-  @Input() card: Card = new Card('', '', '', '');
-  @Input() size: string = 'small';
-  colSpan: string = '';
-
-  ngOnInit(): void {
-      switch (this.size) {
-        case 'small':
-          this.colSpan = 'col-span-1';
-          break;
-        case 'medium':
-          this.colSpan = 'col-span-2';
-          break;
-        case 'large':
-          this.colSpan = 'col-span-3';
-          break;
-        default:
-          this.colSpan = 'col-span-1';
-      }
-  }
-
+export class BentoCardComponent {
+  @Input() card: Card = new Card("", "", "", "");
 }
 
 export class Card {
